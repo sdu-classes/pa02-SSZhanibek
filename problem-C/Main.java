167a180b0f42f3f7d504ec943336f1d54f10d05e
SSZhanibek <124435830+SSZhanibek@users.noreply.github.com>
2023-02-10 20:01:13 +0600

    finish commit

--- problem-C/Main.java
+++ problem-C/Main.java
@@ -71,12 +71,27 @@ class Staff extends Person{
     private double pay;
 
     public Staff(String name ,String address,String school,double pay){
-        super(name,address,school);
+        super(name,address);
         this.name = name;
         this.address = address;
         this.school = school;
         this.pay = pay;
     }
-    public
+    public String getSchool(){
+        return school;
+    }
+    public void setSchool(String school){
+        this.school = school;
+    }
+    public double getPay(){
+        return pay;
+    }
+    public void setPay(double pay){
+        this.pay = pay;
+    }
+    public String toString(){
+        return "Staff[" + super.toString() + "school=" + school + ",pay=" + pay + "]";
+
+    }
 
 }
